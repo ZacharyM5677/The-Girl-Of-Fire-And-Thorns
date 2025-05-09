8cf59e7d6f3fbd68de24b3ae6a35b52e76a2af53
fgdg <zsolardevofficial@gmail.com>
2025-05-08 21:07:15 -0400

    Finally complete!

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.static(__dirname));
+const filePath = path.join(__dirname, 'theGirlOfFireAndThorns.html');
+const data = fs.readFileSync(filePath, 'utf-8');
+
+app.get("/", (req, res) => {
+    res.send(data);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
